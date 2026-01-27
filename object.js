fcf7e93d36a65ea7402de26b4aeacb448e613ec2
Md-Rifat-Pk <mdrifat669900@gmail.com>
2026-01-28 01:52:28 +0600

    Object

---
+++ object.js
@@ -0,0 +1,34 @@
+
+//primitive
+//basic, primary data type
+
+const age = 21;
+const school = 'rifle square';
+const isPassed = true;
+// let isDeveloper;
+const subjects = ['bangla', 'english', 'physics', 'math']
+const bottle = [ 'white', 45];
+const bottleColor = 'white';
+const bottlePrice = 45;
+const bottleDrinks = 'water';
+
+// object
+//non-primitive
+const bottle = {
+    brand: 'apple',
+    price: 45,
+    color: 'white',
+    isClean: false
+}
+
+//non-primitive
+const subject = {
+    name: 'biology',
+    teacher: 'resheda mam',
+    examDate: '30 Feb'
+    chapter: ['first', 'secend', 'third'],
+    exam{
+        name: 'Final Exam'
+        marks: 100
+    }
+}
