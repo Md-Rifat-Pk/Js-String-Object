950d9229aec795a8f3190ffe0f8c8e1dc97662d0
Md-Rifat-Pk <mdrifat669900@gmail.com>
2026-01-27 22:47:17 +0600

    TO UPERCASE & to lowercase compare.trim er used

--- case.js
+++ case.js
@@ -1,3 +1,31 @@
-const school =  'RAJ UK uttara Model School';
+const school = 'RAJ UK uttara Model School';
 
 console.log(school);
+console.log(school.toLowerCase());
+console.log(school.toUpperCase());
+
+
+
+const subject = 'ChemIsTry';
+const book = "chemistry";
+
+
+
+if (subject.toLowerCase === book.toLowerCase){
+    console.log('I am reading book aibar porikkhai fatai felmu');
+}
+
+else {
+    console.log('hudai hudai prista ultai ar vat khawar jounno');
+}
+
+
+const  drink = '     water   ';
+const liquid = 'water'
+
+if(drink.trim === liquid.trim){
+    console.log('Panir Opre Name Jibon');
+}
+else{
+    console.log('Pani tmi Paba na');
+}
