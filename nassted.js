5586270051763dd46065bc5c6c6ae03973911929
MD RIFAT PK <mdrifat669900@gmail.com>
2026-01-28 14:11:14 +0600

    nassted & values

---
+++ nassted.js
@@ -0,0 +1,21 @@
+const collage = {
+    name: 'VNC',
+    class:['11', '12'],
+    events: ['scince fair', 'bijoy dibos', '21 Feb'],
+    unique: {
+        color: "blue",
+        result: {
+            gpa: 5,
+            merit: 'top',
+        }
+    }
+}
+
+// console.log(collage.unique.color)
+collage.unique.result.merit = 'top top top most';
+console.log(collage.unique.result.merit);
+collage.events[1] = '16 December',
+
+console.log(collage.events[1]);
+delete collage.class;
+console.log(collage);
